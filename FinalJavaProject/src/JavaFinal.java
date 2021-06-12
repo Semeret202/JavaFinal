7e473643aa97a4fe048bf1b8e6164950682e5307
semeret202 <semeretshaile@gmail.com>
2021-06-12 18:13:24 -0400

    Java Final Project - First push It includes answers for 4 of the questions

---
+++ FinalJavaProject/src/JavaFinal.java
@@ -0,0 +1,25 @@
+
+public class JavaFinal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 6;
+		int sum = x + y;
+		System.out.println(sum);
+		int z = x * y;
+		System.out.println(z);
+		String str = "Beautifulday";
+		System.out.println(str.length());
+		if (x == y) {
+			System.out.println("Equal");
+		} else {
+			System.out.println("Not");
+		}
+	}
+	
+	
+	}
+	
+
+
